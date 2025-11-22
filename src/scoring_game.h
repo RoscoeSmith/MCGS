6f9849f09f4dc222cf260fd1306e678caafe52cc
Roscoe Smith <roscoe1@ualberta.ca>
2025-11-21 18:39:54 -0700

    tweaked scoring game class

--- src/scoring_game.h
+++ src/scoring_game.h
@@ -1,12 +1,17 @@
+//---------------------------------------------------------------------------
+// A class for score-counting combinatorial games
+//---------------------------------------------------------------------------
 #include "cgt_basics.h"
 #include "game.h"
 
 class scoring_game : public game
 {
 public:
+    virtual int count_score();
 
 protected:
 
 private:
     
-};
+}; // class scoring_game
+
