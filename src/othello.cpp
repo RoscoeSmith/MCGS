b8ee572d1ca3ee362eefe20bac2ae6487317b15f
Roscoe Smith <roscoe1@ualberta.ca>
2025-12-11 19:25:47 -0700

    othello

---
+++ src/othello.cpp
@@ -0,0 +1,88 @@
+#include "othello.h"
+#include "cgt_basics.h"
+#include "cgt_move.h"
+#include "game.h"
+#include "grid.h"
+
+using namespace std;
+
+
+class othello_move_generator : public move_generator
+{
+public:
+    othello_move_generator(const othello& game, bw to_play);
+
+    void operator++() override;
+    operator bool() const override;
+    ::move gen_move() const override;
+
+private:
+    const othello& _game;
+    int _current; // current stone location to test
+};
+
+othello_move_generator::othello_move_generator(const othello& game, bw to_play)
+    : move_generator(to_play),
+    _game(game)
+{
+    
+}
+
+void othello_move_generator::operator++()
+{
+    
+}
+
+othello_move_generator::operator bool() const
+{
+    return false;
+}
+
+::move othello_move_generator::gen_move() const
+{
+    return 0;
+}
+
+
+othello::othello(int n_rows, int n_cols) : grid(n_rows, n_cols)
+{
+}
+
+othello::othello(const vector<int>& board, int_pair shape) : grid(board, shape)
+{
+}
+
+othello::othello(const string& game_as_string) : grid(game_as_string)
+{
+}
+
+void othello::play(const ::move& m, bw to_play)
+{
+    grid::play(m, to_play);
+
+    const int to = m;
+    assert(at(to) == EMPTY);
+
+    replace(to, to_play);
+
+    const bw opp = opponent(to_play);
+}
+
+void othello::undo_move()
+{
+    const ::move mc = grid::last_move();
+    grid::undo_move();
+
+    const int to = cgt_move::decode(mc);
+    const bw player = cgt_move::get_color(mc);
+    assert(at(to) == player);
+    const int N = size();
+
+    replace(to, EMPTY);
+    
+}
+
+move_generator* othello::create_move_generator(bw to_play) const
+{
+    return new othello_move_generator(*this, to_play);
+}
---
+++ src/othello.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "game.h"
+#include "scoring_game.h"
+#include "grid.h"
+
+class othello : public scoring_game, public grid
+{
+public:
+    othello(int n_rows, int n_cols);
+    othello(const std::vector<int>& board, int_pair shape);
+    othello(const std::string& game_as_string);
+
+    void play(const move& m, bw to_play) override;
+    void undo_move() override;
+
+    move_generator* create_move_generator(bw to_play) const override;
+};
+
+
