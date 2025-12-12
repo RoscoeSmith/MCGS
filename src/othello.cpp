1582c8d0d9cd092020b4247c9a9c14dc5a2a2f08
SAMK6 <sam.feldman74@gmail.com>
2025-12-11 21:29:35 -0700

    added movegen stuff

--- src/othello.cpp
+++ src/othello.cpp
@@ -23,6 +23,8 @@ private:
     const othello& _game;
     int _current; // current stone location to test
     bool _has_move;
+    bool _north, _north_east, _east, _south_east, _south, _south_west, _west, _north_west;
+
 };
 
 othello_move_generator::othello_move_generator(const othello& game, bw to_play)
@@ -30,7 +32,17 @@ othello_move_generator::othello_move_generator(const othello& game, bw to_play)
     _game(game),
 {
     _current = -1;
-    _hash_move = false;
+    _has_move = false;
+
+    _south = false;
+    _north = false;
+    _east = false;
+    _west = false;
+    _south_east = false;
+    _south_west = false;
+    _north_east = false;
+    _north_west = false;
+
     _next_move(true);
     
 }
@@ -48,7 +60,26 @@ othello_move_generator::operator bool() const
 
 ::move othello_move_generator::gen_move() const
 {
-    return _current;
+    assert(*this)
+
+    ::move oth_move = 0;
+
+    // pass move
+    if(!(_south || _north || _east || _west || _south_east || _south_west || _north_east || _north_west))
+        return oth_move;
+
+    oth_move |= _current;
+
+    oth_move |= (_south ? 1 : 0) << 29;
+    oth_move |= (_north ? 1 : 0) << 28;
+    oth_move |= (_east ? 1 : 0) << 27;
+    oth_move |= (_west ? 1 : 0) << 26;
+    oth_move |= (_south_east ? 1 : 0) << 25;
+    oth_move |= (_south_west ? 1 : 0) << 24;
+    oth_move |= (_north_east ? 1 : 0) << 23;
+    oth_move |= (_north_west ? 1 : 0) << 22;
+
+    return oth_move;
 }
 
 void othello_move_generator::_next_move(bool inti)
@@ -58,21 +89,245 @@ void othello_move_generator::_next_move(bool inti)
 
     _has_move = false;
 
+    _south = false;
+    _north = false;
+    _east = false;
+    _west = false;
+    _south_east = false;
+    _south_west = false;
+    _north_east = false;
+    _north_west = false;
+
+
     _current ++;
 
-    int_pair cord;
+    int_pair rc;
+
+    bool should_capture = false;
+    int cap_size = 0;
 
     while(_current < _game.size())
     {
 
-        cord = point_to_coord(_current);
-
-        for(int i = cord.first + 1; coord_in_bounds({cord.first, i}), i++)
+        if(_game.at(_current) != EMPTY)
         {
+            _current ++;
+            continue;
+        }
 
+        rc = point_to_coord(_current);
+
+        // inc row
+        for (int r = rc.first + 1; coord_in_bounds(int_pair(r, rc.second)); r++)
+        {
+            int coord = coord_to_point(int_pair(r, rc.second));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _south = true;
+        }
+
+
+        // dec row
+        should_capture = false;
+        cap_size = 0;
+        for (int r = rc.first - 1; coord_in_bounds(int_pair(r, rc.second)); r--)
+        {
+            int coord = coord_to_point(int_pair(r, rc.second));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _north = true;
+        }
+
+        // inc col
+        should_capture = false;
+        cap_size = 0;
+        for (int c = rc.second + 1; coord_in_bounds(int_pair(rc.first, c)); c++)
+        {
+            int coord = coord_to_point(int_pair(rc.first, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _east = true;
+        }
+
+        // dec col
+        should_capture = false;
+        cap_size = 0;
+        for (int c = rc.second - 1; coord_in_bounds(int_pair(rc.first, c)); c--)
+        {
+            int coord = coord_to_point(int_pair(rc.first, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _west = true;
+        }
+
+        // inc row and col
+        should_capture = false;
+        cap_size = 0;
+        for (int r = rc.first + 1, c = rc.second + 1; coord_in_bounds(int_pair(r, c)); r++, c++)
+        {
+            int coord = coord_to_point(int_pair(r, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _south_east = true;
+        }
+
+        // inc row, dec col
+        should_capture = false;
+        cap_size = 0;
+        for (int r = rc.first + 1, c = rc.second - 1; coord_in_bounds(int_pair(r, c)); r++, c--)
+        {
+            int coord = coord_to_point(int_pair(r, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _south_west = true;
+        }
+
+        // dec row, inc col
+        should_capture = false;
+        cap_size = 0;
+        for (int r = rc.first - 1, c = rc.second + 1; coord_in_bounds(int_pair(r, c)); r--, c++)
+        {
+            int coord = coord_to_point(int_pair(r, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _north_east = true;
+        }
+
+        // dec row and col
+        should_capture = false;
+        cap_size = 0;
+        for (int r = rc.first - 1, c = rc.second - 1; coord_in_bounds(int_pair(r, c)); r--, c--)
+        {
+            int coord = coord_to_point(int_pair(r, c));
+            if (at(coord) == opp)
+            {
+                cap_size ++;
+            }
+            else if (at(coord) == to_play)
+            {
+                should_capture = true;
+                break;
+            }
+            else if (at(coord) == EMPTY)
+            {
+                break;
+            }
+        }
+        if (should_capture and cap_size >= 1)
+        {
+            _north_west = true;
+        }
+
+        if(_south || _north || _east || _west || _south_east || _south_west || _north_east || _north_west)
+        {
+            _has_move = true;
+            break;
         }
 
         _current ++;
+    }
+
+    if(init && !_has_move)
+    {
+        _has_move = true;
     }
     
 }
